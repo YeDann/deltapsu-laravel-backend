2638fe1964941b77acc0e802fa255329c6a6e62d
chai <chai@degitobangkok.com>
2020-08-28 14:40:03 +0700

    fix date

--- dependencies/storage/framework/views/abf6e0fad80e8e66aa9f1a2e228b0fc26fe79ddf.php
+++ dependencies/storage/framework/views/abf6e0fad80e8e66aa9f1a2e228b0fc26fe79ddf.php
@@ -1,8 +1,7 @@
-@extends('layouts.admin')
-@section('style')
+<?php $__env->startSection('style'); ?>
 
-@endsection
-@section('content')
+<?php $__env->stopSection(); ?>
+<?php $__env->startSection('content'); ?>
 <!-- Nav -->
 <div class="bg-body-light">
     <div class="content content-full">
@@ -10,7 +9,7 @@
             <h1 class="flex-sm-fill font-size-h2 font-w400 mt-2 mb-0 mb-sm-2">Edit Document</h1>
             <nav class="flex-sm-00-auto ml-sm-3" aria-label="breadcrumb">
                 <ol class="breadcrumb">
-                    <li class="breadcrumb-item"> <a href="{{route('docFilerBy' , $docs[0]->cate_id)}}">Documents</a></li>
+                    <li class="breadcrumb-item"> <a href="<?php echo e(route('docFilerBy' , $docs[0]->cate_id)); ?>">Documents</a></li>
                     <li class="breadcrumb-item active" aria-current="page">Create</li>
                 </ol>
             </nav>
@@ -19,26 +18,29 @@
 </div>
 <!-- Content -->
 <div class="content">
-    @if(Session::has('flash_message'))
+    <?php if(Session::has('flash_message')): ?>
     <div class="alert alert-success" role="alert">
         <button class="close" data-dismiss="alert"></button>
-        {!! Session('flash_message') !!}
+        <?php echo Session('flash_message'); ?>
+
     </div>
-    @endif
-    @if(Session::has('error_message'))
+    <?php endif; ?>
+    <?php if(Session::has('error_message')): ?>
     <div class="alert alert-danger" role="alert">
         <button class="close" data-dismiss="alert"></button>
-        {!! Session('error_message') !!}
+        <?php echo Session('error_message'); ?>
+
     </div>
-    @endif
+    <?php endif; ?>
     <div class="block block-rounded block-bordered">
         <div class="block-header block-header-default">
             <h3 class="block-title">Document</h3>
         </div>
         <div class="block-content">
-            <form action="{{route('updateProdoc')}}" method="POST" enctype="multipart/form-data">
-                {{csrf_field()}}
-            <input type="hidden" name="doc_id" value="{{$docs[0]->doc_id}}">
+            <form action="<?php echo e(route('updateProdoc')); ?>" method="POST" enctype="multipart/form-data">
+                <?php echo e(csrf_field()); ?>
+
+            <input type="hidden" name="doc_id" value="<?php echo e($docs[0]->doc_id); ?>">
                 <!-- Basic Elements -->
                 <div class="row">
                     <div class="col-lg-12">
@@ -46,27 +48,27 @@
                                     <label for="example-select"><span class="req-fed">*</span> Select Document Types</label>
                             <select class="js-select2 form-control" id="catedocId" onchange="selectDocCate();" name="doc_categories" data-placeholder="Choose one.." required>
                                 <option></option>
-                                @foreach ($categories as $main)
-                                @if($main->id == $docs[0]->cate_id)
-                                <option value="{{$main->id}}" selected>{{$main->title}}</option>
-                                @else 
-                                <option value="{{$main->id}}">{{$main->title}}</option>
-                                @endif
-                                @endforeach
+                                <?php $__currentLoopData = $categories; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $main): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                                <?php if($main->id == $docs[0]->cate_id): ?>
+                                <option value="<?php echo e($main->id); ?>" selected><?php echo e($main->title); ?></option>
+                                <?php else: ?> 
+                                <option value="<?php echo e($main->id); ?>"><?php echo e($main->title); ?></option>
+                                <?php endif; ?>
+                                <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
                                    
                             </select>
                                 </div>
                         <div class="block block-rounded block-bordered">
                             <ul class="nav nav-tabs nav-tabs-alt" data-toggle="tabs" role="tablist">
-                                @foreach ($language as $item)
+                                <?php $__currentLoopData = $language; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $item): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
                                 <li class="nav-item">
-                                <a class="nav-link {{$loop->iteration == 1 ?'active':''}}" href="#btabs-alt-static-{{$item->name}}"
-                                        style="text-transform: capitalize;">{{$item->name}}</a>
+                                <a class="nav-link <?php echo e($loop->iteration == 1 ?'active':''); ?>" href="#btabs-alt-static-<?php echo e($item->name); ?>"
+                                        style="text-transform: capitalize;"><?php echo e($item->name); ?></a>
                                 </li>
-                                @endforeach
+                                <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
                             </ul>
                             <div class="block-content tab-content">
-                                @foreach ($language as $item2)
+                                <?php $__currentLoopData = $language; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $item2): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
                                 <?php 
                                 $current = null;
                                 foreach($docs as $item) { 
@@ -76,46 +78,46 @@
                                     }
                                 }
                                ?>
-                                <input type="hidden" name="lang_loop[]" value="{{$item2->name}}">
-                                <div class="tab-pane {{($loop->iteration == 1)? 'active':''}}" id="btabs-alt-static-{{$item2->name}}" role="tabpanel">
+                                <input type="hidden" name="lang_loop[]" value="<?php echo e($item2->name); ?>">
+                                <div class="tab-pane <?php echo e(($loop->iteration == 1)? 'active':''); ?>" id="btabs-alt-static-<?php echo e($item2->name); ?>" role="tabpanel">
                                         <div class="form-group">
                                             <label for="example-select">Name</label>
                                             <input type="text"
-                                                class="form-control {{ $errors->has('name') ? 'is-invalid' : '' }}"
-                                                name="name[{{$item2->name}}]" value="{{isset($current->name)? $current->name :'' }}" placeholder="Enter name...">
+                                                class="form-control <?php echo e($errors->has('name') ? 'is-invalid' : ''); ?>"
+                                                name="name[<?php echo e($item2->name); ?>]" value="<?php echo e(isset($current->name)? $current->name :''); ?>" placeholder="Enter name...">
                                         </div>
                                         <div class="form-group">
                                                 <label for="example-select"> Old File</label>
-                                                <a href="{{config('app.url')}}/upload/product_files/{{isset($current->file)? $current->file :'' }}"
-                                                    target="_blank">{{isset($current->file)? $current->file :'' }}</a>
-                                                    <input type="hidden" name="oldfile[{{$item2->name}}]"  value="{{isset($current->file)? $current->file :'' }}" >
-                                                    @if(isset($current->file))
-                                                    <a href="{{route('removefileDoc',[$item2->name,$docs[0]->doc_fk_id])}}" target="_blank" class="btn btn btn-danger"><i class="fa fa-trash"></i> </a>
-                                                        @endif
+                                                <a href="<?php echo e(config('app.url')); ?>/upload/product_files/<?php echo e(isset($current->file)? $current->file :''); ?>"
+                                                    target="_blank"><?php echo e(isset($current->file)? $current->file :''); ?></a>
+                                                    <input type="hidden" name="oldfile[<?php echo e($item2->name); ?>]"  value="<?php echo e(isset($current->file)? $current->file :''); ?>" >
+                                                    <?php if(isset($current->file)): ?>
+                                                    <a href="<?php echo e(route('removefileDoc',[$item2->name,$docs[0]->doc_fk_id])); ?>" target="_blank" class="btn btn btn-danger"><i class="fa fa-trash"></i> </a>
+                                                        <?php endif; ?>
                                             </div>
                                         <div class="form-group">
                                             <label for="example-select">New File <span class="req-fed">* Max File Size 20 MB</span></label>
                                             <div class="custom-file " style="width:100%;">
-                                                <input type="file" class="custom-file-input" name="fileGU[{{$item2->name}}]"
+                                                <input type="file" class="custom-file-input" name="fileGU[<?php echo e($item2->name); ?>]"
                                                     data-toggle="custom-file-input">
                                                 <label class="custom-file-label" for="fileImage">Choose file</label>
                                             </div>
                                         </div>
                                     </div>
-                                @endforeach
+                                <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
                             </div>
                         </div>
                         <div  class="block block-rounded block-bordered">
                             <ul class="nav nav-tabs nav-tabs-alt" data-toggle="tabs" role="tablist">
-                                @foreach ($other_lang as $item)
+                                <?php $__currentLoopData = $other_lang; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $item): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
                                 <li class="nav-item">
-                                <a class="nav-link {{$loop->iteration == 1 ?'active':''}}" href="#btabs-alt-static-{{$item->name}}"
-                                        style="text-transform: capitalize;">{{$item->name}}</a>
+                                <a class="nav-link <?php echo e($loop->iteration == 1 ?'active':''); ?>" href="#btabs-alt-static-<?php echo e($item->name); ?>"
+                                        style="text-transform: capitalize;"><?php echo e($item->name); ?></a>
                                 </li>
-                                @endforeach
+                                <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
                             </ul>
                             <div class="block-content tab-content">
-                                @foreach ($other_lang as $item3)
+                                <?php $__currentLoopData = $other_lang; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $item3): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
                                 <?php 
                                 $current2 = null;
                                 foreach($docs as $item) { 
@@ -126,34 +128,34 @@
                                 }
                                ?>
                              
-                                <input type="hidden" name="lang_loop[]" value="{{$item3->name}}">
-                                <div class="tab-pane {{($loop->iteration == 1)? 'active':''}}" id="btabs-alt-static-{{$item3->name}}" role="tabpanel">
+                                <input type="hidden" name="lang_loop[]" value="<?php echo e($item3->name); ?>">
+                                <div class="tab-pane <?php echo e(($loop->iteration == 1)? 'active':''); ?>" id="btabs-alt-static-<?php echo e($item3->name); ?>" role="tabpanel">
                                         <div class="form-group">
                                             <label for="example-select">Name</label>
                                             <input type="text"
-                                                class="form-control {{ $errors->has('name') ? 'is-invalid' : '' }}"
-                                                name="name[{{$item3->name}}]" value="{{isset($current2->name)? $current2->name :'' }}" placeholder="Enter name...">
+                                                class="form-control <?php echo e($errors->has('name') ? 'is-invalid' : ''); ?>"
+                                                name="name[<?php echo e($item3->name); ?>]" value="<?php echo e(isset($current2->name)? $current2->name :''); ?>" placeholder="Enter name...">
                                         </div>
                                         <div class="form-group">
                                                 <label for="example-select"> Old File</label>
-                                                <a href="{{config('app.url')}}/upload/product_files/{{isset($current2->file)? $current2->file :'' }}"
-                                                    target="_blank">{{isset($current2->file)? $current2->file :'' }}</a>
-                                                    <input type="hidden" name="oldfile[{{$item3->name}}]"  value="{{isset($current2->file)? $current2->file :'' }}" >
-                                                    @if(isset($current2->file))
-                                                    <a href="{{route('removefileDoc',[$item3->name,$docs[0]->doc_fk_id])}}" target="_blank" class="btn btn btn-danger"><i class="fa fa-trash"></i> </a>
-                                                        @endif
+                                                <a href="<?php echo e(config('app.url')); ?>/upload/product_files/<?php echo e(isset($current2->file)? $current2->file :''); ?>"
+                                                    target="_blank"><?php echo e(isset($current2->file)? $current2->file :''); ?></a>
+                                                    <input type="hidden" name="oldfile[<?php echo e($item3->name); ?>]"  value="<?php echo e(isset($current2->file)? $current2->file :''); ?>" >
+                                                    <?php if(isset($current2->file)): ?>
+                                                    <a href="<?php echo e(route('removefileDoc',[$item3->name,$docs[0]->doc_fk_id])); ?>" target="_blank" class="btn btn btn-danger"><i class="fa fa-trash"></i> </a>
+                                                        <?php endif; ?>
                                                 </div>
                                                 
                                         <div class="form-group">
                                             <label for="example-select">New File <span class="req-fed">* Max File Size 20 MB</span></label>
                                             <div class="custom-file " style="width:100%;">
-                                                <input type="file"  class="custom-file-input" name="fileGU[{{$item3->name}}]"
+                                                <input type="file"  class="custom-file-input" name="fileGU[<?php echo e($item3->name); ?>]"
                                                     data-toggle="custom-file-input">
                                                 <label class="custom-file-label" for="fileImage">Choose file</label>
                                             </div>
                                         </div>
                                     </div>
-                                @endforeach
+                                <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
                             </div>
                         </div>
                     </div>
@@ -164,9 +166,7 @@
                                     <select class="js-select2 form-control" id="ModelIdNotSelectIncate" name="product[]" style="width: 100%;" data-placeholder="Choose many.." multiple>
                                         <option></option><!-- Required for data-placeholder attribute to work with Select2 plugin -->
                                       
-                                        {{-- @foreach ($doc_has_pros as $item)
-                                        <option value="{{$item->pro_id}}" selected>{{$item->pro_code}}</option>
-                                        @endforeach --}}
+                                        
                                        
                                     </select>
                                 </div>
@@ -175,7 +175,7 @@
                     <div class="col-lg-12 mt-5">
                         <div class="text-center form-group">
                             <button class="btn btn-info" type="submit">Update </button>
-                            <a href="{{route('docFilerBy' , $docs[0]->cate_id)}}" class="btn btn-secondary">
+                            <a href="<?php echo e(route('docFilerBy' , $docs[0]->cate_id)); ?>" class="btn btn-secondary">
                                 Cancel
                             </a>
                         </div>
@@ -185,8 +185,8 @@
         </div>
     </div>
 </div>
-@endsection
-@section('js')
+<?php $__env->stopSection(); ?>
+<?php $__env->startSection('js'); ?>
 <script>
     $(document).on('change', '.custom-file-input', function () {
         // alert(this.files[0].size);
@@ -206,7 +206,7 @@
       var cateid = $("#catedocId").val();
       console.log(cateid);
         $.ajax({
-            url: "{{ (route('searhModelProductByCatedoc'))}}",
+            url: "<?php echo e((route('searhModelProductByCatedoc'))); ?>",
             data: {
             'cateid': cateid,
            },
@@ -235,4 +235,6 @@
     }
 
 </script>
-@endsection
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.admin', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\Deltapsu_Production\dependencies\resources\views/product_doc/edit_doc.blade.php ENDPATH**/ ?>
