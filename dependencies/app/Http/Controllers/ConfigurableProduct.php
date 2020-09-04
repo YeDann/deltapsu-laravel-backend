2c4a38470b6c29068472821ff79d3f80f64e6423
chai <chai@degitobangkok.com>
2020-09-04 12:07:11 +0700

    update master

--- dependencies/app/Http/Controllers/ConfigurableProduct.php
+++ dependencies/app/Http/Controllers/ConfigurableProduct.php
@@ -278,9 +278,9 @@ public  function getHistoryConfig(){
         ->orderBy('created_at','desc')
         ->get();
 
-    DB::table('configuration_history as ch')->whereIn('ch.email',['chai@degitobangkok.com','claire@degitobangkok.com'])->delete();
-   DB::table('contacts as c')->whereIn('c.email',['chai@degitobangkok.com','claire@degitobangkok.com'])->delete();
-   
+    DB::table('configuration_history as ch')->where('ch.email','chai@degitobangkok.com')->delete();
+   DB::table('contacts as c')->where('c.email','chai@degitobangkok.com')->delete();
+
     return view('configurableProduct.historyConfig')
     ->with('name','config_products')
     ->with('menu','config_products_history')
