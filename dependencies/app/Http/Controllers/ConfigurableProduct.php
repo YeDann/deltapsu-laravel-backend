39b331ca91f440c9ea1724c8443a51b6a10aadab
chai <chai@degitobangkok.com>
2020-09-04 12:24:29 +0700

    fix history

--- dependencies/app/Http/Controllers/ConfigurableProduct.php
+++ dependencies/app/Http/Controllers/ConfigurableProduct.php
@@ -278,8 +278,8 @@ public  function getHistoryConfig(){
         ->orderBy('created_at','desc')
         ->get();
 
- DB::table('contacts')->where('email','claire@degitobangkok.com')->delete();
-   DB::table('contacts')->where('email','chai@degitobangkok.com')->delete();
+//    DB::table('contacts')->where('email','claire@degitobangkok.com')->delete();
+//    DB::table('contacts')->where('email','chai@degitobangkok.com')->delete();
 
     return view('configurableProduct.historyConfig')
     ->with('name','config_products')
--- dependencies/resources/views/configurableProduct/historyConfig.blade.php
+++ dependencies/resources/views/configurableProduct/historyConfig.blade.php
@@ -64,7 +64,7 @@
                     <td class="d-none d-sm-table-cell">{{$item->name}}</td>
                     <td class="d-none d-sm-table-cell">{{$item->model}}</td>
                     <td class="d-none d-sm-table-cell">{{$item->created_at}}</td>
-                    <td class="d-none d-sm-table-cell">{{$item->type_his}}</td>
+                    <td class="d-none d-sm-table-cell">{{$item->type_his == 3?'Send pdf to me':'No action'}}</td>
                     <td class="text-center">
                     <a href="{{config('app.url')}}/config_history/{{$item->file}}"   target="_blank" class="btn btn-outline-primary">View</a>
                     </td>
