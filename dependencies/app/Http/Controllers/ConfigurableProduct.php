6a35e6cf56ea37fc67f3eaccba86aa876c61b9a9
chai <chai@degitobangkok.com>
2020-09-04 12:18:12 +0700

    update delte feedback

--- dependencies/app/Http/Controllers/ConfigurableProduct.php
+++ dependencies/app/Http/Controllers/ConfigurableProduct.php
@@ -278,8 +278,8 @@ public  function getHistoryConfig(){
         ->orderBy('created_at','desc')
         ->get();
 
- DB::table('configuration_history')->where('email','claire@degitobangkok.com')->delete();
-//    DB::table('contacts as c')->where('c.email','chai@degitobangkok.com')->delete();
+ DB::table('contacts')->where('email','claire@degitobangkok.com')->delete();
+   DB::table('contacts')->where('email','chai@degitobangkok.com')->delete();
 
     return view('configurableProduct.historyConfig')
     ->with('name','config_products')
