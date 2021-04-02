eb5f4e7cf5a6d8a6d056496a9d48a97e53215642
chai <chai@degitobangkok.com>
2021-04-02 14:00:27 +0700

    update btn access

--- dependencies/resources/views/layouts/footer.blade.php
+++ dependencies/resources/views/layouts/footer.blade.php
@@ -443,7 +443,7 @@
            <div class="col-lg-12 p-3 text-center">
             {{$staticContent['We_use_cookies_to_provide']}}
             <a href="{{route('privacyPolicy')}}" class="text-underline text-bold"> {{$staticContent['Privacy_Policy']}}</a>.</p>
-              <div class="btn btn-subscribe" onclick="setcokie();" href="#">{{$staticContent['Accept']}}</div>
+              <a class="btn btn-subscribe" onclick="setcokie();" href="#">{{$staticContent['Accept']}}</a>
            </div>
        </div>
     </div>
