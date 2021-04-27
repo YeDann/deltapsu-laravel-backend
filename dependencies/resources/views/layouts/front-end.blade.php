62717af8ae4edc8003102984f7651e8b07a2842d
chai <chai@degitobangkok.com>
2021-04-27 15:58:00 +0700

    update GTM

--- dependencies/resources/views/layouts/front-end.blade.php
+++ dependencies/resources/views/layouts/front-end.blade.php
@@ -167,6 +167,20 @@
         gtag('js', new Date());
         gtag('config', 'UA-67607418-1');
       </script>
+
+      <!-- Google Tag Manager -->
+        <script>
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            
+            })(window,document,'script','dataLayer','GTM-PBXMMSN');
+        </script>
+      <!-- End Google Tag Manager -->
     
 </head>
 <body>
@@ -213,6 +227,14 @@ async defer>
  });
 
 </script>
+
+  <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PBXMMSN"
+
+      height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+      
+      <!-- End Google Tag Manager (noscript) -->
+  
     @yield('js')
     <script>
        $(document).ready(function() {
