b04b336bc065ac328de67711caa35c0dfa38a1f5
chai <chai@degitobangkok.com>
2021-04-27 16:03:51 +0700

    update GTM 2

--- dependencies/resources/views/layouts/header-front.blade.php
+++ dependencies/resources/views/layouts/header-front.blade.php
@@ -1046,7 +1046,12 @@ top: -9px;
       </div>
     </div>
   </div>
+ <!-- Google Tag Manager (noscript) -->
+ <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PBXMMSN"
 
+  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+  
+  <!-- End Google Tag Manager (noscript) -->
  
   
 
