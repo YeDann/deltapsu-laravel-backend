1c8a7499b6f1ef5ca3495fb900390a4344cca44c
chai <chaisa097@gamil.com>
2021-11-15 14:17:21 +0700

    getExportOldProduct

--- dependencies/resources/views/product/index.blade.php
+++ dependencies/resources/views/product/index.blade.php
@@ -52,7 +52,6 @@
                        {{-- <a href="{{route('getExcelProTag')}}" class="btn btn-outline-primary">Import Product Tags</a> --}}
 
                     <a href="{{route('getExportProduct')}}" class="btn btn-outline-primary">Export Products</a>
-                    <!-- <a href="{{route('getExportOldProduct')}}" class="btn btn-outline-primary">Export Old Products</a> -->
                     
                 </div>
             </div>
