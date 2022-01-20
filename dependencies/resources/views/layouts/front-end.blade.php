b0802cc7ed22b002212c3bc8ca2c455c52de26a9
chai <chaisa097@gamil.com>
2022-01-20 16:12:00 +0700

    update frontend

--- dependencies/resources/views/layouts/front-end.blade.php
+++ dependencies/resources/views/layouts/front-end.blade.php
@@ -185,9 +185,10 @@
     @include('layouts.header-front')
     @yield('container')
     @include('layouts.footer')
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.7.5/js/bootstrap-select.min.js"></script>
+
+<script type="text/javascript" src="{{asset('/frontend-asset/js/popper.min.js')}}" ></script>
+<script type="text/javascript" src="{{asset('/frontend-asset/js/bootstrap.min.js')}}" ></script>
+<script type="text/javascript" src="{{asset('/frontend-asset/js/bootstrap-select.min.js')}}"></script>
 <script src="{{asset('/frontend-asset/js/map.js')}}"></script>
 <script src="{{asset('/frontend-asset/js/product.js')}}"></script>
 <script src="{{asset('/frontend-asset/js/owl.carousel.js')}}"></script>
