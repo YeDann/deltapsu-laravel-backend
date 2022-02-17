8c6b2da4a69c98a0439f1c785b47570470470499
chai <chaisa097@gamil.com>
2022-02-17 18:49:52 +0700

    fix web data

--- dependencies/routes/web.php
+++ dependencies/routes/web.php
@@ -28,7 +28,7 @@ Route::get('/products/download/{cate_name?}/{modelname?}','FrontendController@do
 Route::get('/products/download/{lang?}/{cate_name?}/{modelname?}','FrontendController@downloadFIleManual')->name('downloadFIleManual');
 Route::group([
      'prefix' => LaravelLocalization::setLocale(),
-    'middleware' => ['localeSessionRedirect', 'localizationRedirect', 'localeViewPath','cors']
+    'middleware' => ['localeSessionRedirect', 'localizationRedirect', 'localeViewPath']
         ], function() {
 
 Route::get('/{page?}','FrontendController@index')->name('index');
