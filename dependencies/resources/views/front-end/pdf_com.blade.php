a94257c247d8f0b6b3c325495d7aa2d3ab53b4c1
chai <dev@degitobangkok.com>
2022-07-05 14:34:40 +0700

    update pdf compare

--- dependencies/resources/views/front-end/pdf_com.blade.php
+++ dependencies/resources/views/front-end/pdf_com.blade.php
@@ -77,10 +77,14 @@ tbody >tr > td:first-child{
 }
  </style>
    <style type="text/css">
-   
+    @font-face {
+        font-family: "DeltaSansSan";
+       src: url("../fonts/DeltaSans-Regular.woff") format('woff'),
+       url('../fonts/DeltaSans-Regular.woff2') format('woff2');
+    }
     
     * {
-        font-family: 'DeltaSans';
+      font-family:"DeltaSansSan";
     }
     </style>
 </head>
