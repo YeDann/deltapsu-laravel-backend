efb2e5fdcc15ef72a59013ecef168e9ada062e97
chai <dev@degitobangkok.com>
2022-08-19 14:31:57 +0700

    update

--- dependencies/app/Http/Controllers/FrontendController.php
+++ dependencies/app/Http/Controllers/FrontendController.php
@@ -3548,7 +3548,7 @@ class FrontendController extends Controller
          
            try {
         
-            $emaillog = Mail::to($emailTest)->send(new Contact($request->except('_token'),$ticket_id));
+            $emaillog = Mail::to($emailsend)->send(new Contact($request->except('_token'),$ticket_id));
             Log::channel('mail_log')->info('[Success] message : Send Mail to '.implode(",",$emailsend));
             return \Redirect::back()->with("message","Send Email Successfully");
            } catch (\Swift_RfcComplianceException  $ex) {
