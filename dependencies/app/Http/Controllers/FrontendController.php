268964422bf1a9c625b38e96c7d8ebc13c910bac
chai <dev@degitobangkok.com>
2022-08-24 14:50:24 +0700

    update email

--- dependencies/app/Http/Controllers/FrontendController.php
+++ dependencies/app/Http/Controllers/FrontendController.php
@@ -3542,8 +3542,8 @@ class FrontendController extends Controller
           }
          
            try {
-            $testemail = "chai@degitobangkok.com";
-            $emaillog = Mail::to($testemail)->send(new Contact($request->except('_token'),$ticket_id));
+           
+            $emaillog = Mail::to($emailsend)->send(new Contact($request->except('_token'),$ticket_id));
             Log::channel('mail_log')->info('[Success] message : Send Mail to '.implode(",",$emailsend));
             return \Redirect::back()->with("message","Send Email Successfully");
            } catch (\Swift_RfcComplianceException  $ex) {
