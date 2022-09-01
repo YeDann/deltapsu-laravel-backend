1f21efa3f71304e4565e5e4144747f9dc18052a5
chai <dev@degitobangkok.com>
2022-09-01 16:52:23 +0700

    update

--- dependencies/storage/framework/views/4037b139a5b05d6ed69324ee3a7f7584a9806a68.php
+++ dependencies/storage/framework/views/4037b139a5b05d6ed69324ee3a7f7584a9806a68.php
@@ -187,7 +187,7 @@
 <script type="text/javascript" src="https://www.recaptcha.net/recaptcha/api.js?onload=onloadCallback&render=explicit&hl=en"
 async defer>
 </script>
-<script id="Cookiebot" src="https://consent.cookiebot.com/uc.js" data-cbid="0b87f4dd-13cf-473e-b5c7-019f4154d02a" data-blockingmode="auto" type="text/javascript"></script> 
+<script id="Cookiebot"  data-culture="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>" src="https://consent.cookiebot.com/uc.js" data-cbid="0b87f4dd-13cf-473e-b5c7-019f4154d02a" data-blockingmode="auto" type="text/javascript"></script> 
 <script id="CookieDeclaration" src="https://consent.cookiebot.com/0b87f4dd-13cf-473e-b5c7-019f4154d02a/cd.js" type="text/javascript" async></script> 
 
 <script>
