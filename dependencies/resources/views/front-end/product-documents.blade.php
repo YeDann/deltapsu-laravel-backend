67d6b99be256e85a8dec11f1423504bfc7709fe1
chai <dev@degitobangkok.com>
2022-09-02 16:56:53 +0700

    update

--- dependencies/resources/views/front-end/product-documents.blade.php
+++ dependencies/resources/views/front-end/product-documents.blade.php
@@ -357,7 +357,7 @@
         //    });
           var findPro =  products.filter(function(e) { return e.pro_code === key});
           var findByTag =  tags_data.filter(function(x) { return  x.tag.toLowerCase().replace(/\s/g, '') === key.toLowerCase().replace(/\s/g, '') });
-          console.log(findByTag.length ,'findByTag.length',tags_data ,findByTag);
+          console.log(findByTag.length ,'findByTag.length' ,findByTag);
           if(findPro.length > 0){
             $('#model_id_key').val(findPro[0]["product_id"]); 
             loadContent(2);
