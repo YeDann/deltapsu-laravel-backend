e63f02d485526cb2107c1da35c426e3e219f6647
chai <dev@degitobangkok.com>
2022-09-06 09:50:22 +0700

    update

--- dependencies/resources/views/layouts/header-front.blade.php
+++ dependencies/resources/views/layouts/header-front.blade.php
@@ -898,7 +898,7 @@ top: -9px;
         </div>
         <div class="modal-body px-4 mb-4">
             <div class="text-center">
-                <h4 class="text-color-delta">Sorry, Please verify im'not robot</h4>
+                <h4 class="text-color-delta">Please verify you are not a robot</h4>
             </div>
         </div>
       </div>
