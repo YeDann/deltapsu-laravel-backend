0c764da724bae4e2e59391eb3553d43fd84a2cba
chai <dev@degitobangkok.com>
2022-09-07 09:53:22 +0700

    update

--- dependencies/resources/views/layouts/front-end.blade.php
+++ dependencies/resources/views/layouts/front-end.blade.php
@@ -112,9 +112,7 @@
       #CookiebotWidget .CookiebotWidget-body .CookiebotWidget-main-logo {
          display: none !important;
       }
-      .CookieDeclaration{
-        display: none !important;
-      }
+   
       #CybotCookiebotDialogHeader {
          width: 0px !important;
      }
@@ -221,8 +219,6 @@ $langch = str_replace('_', '-', app()->getLocale());
 
 <script id="Cookiebot"  data-culture="{{$lanngCookie}}" src="https://consent.cookiebot.com/uc.js" data-cbid="0b87f4dd-13cf-473e-b5c7-019f4154d02a" data-blockingmode="auto" type="text/javascript"></script> 
 
-
-<script id="CookieDeclaration" src="https://consent.cookiebot.com/0b87f4dd-13cf-473e-b5c7-019f4154d02a/cd.js" type="text/javascript" async></script> 
 
 <script>
   if ('loading' in HTMLImageElement.prototype) {
