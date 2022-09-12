60dc067829f9c71d788e1bd7dfc4e97c02e461d6
chai <dev@degitobangkok.com>
2022-09-12 10:19:27 +0700

    chage key download gui

--- dependencies/app/Http/Controllers/FrontendController.php
+++ dependencies/app/Http/Controllers/FrontendController.php
@@ -3607,8 +3607,8 @@ class FrontendController extends Controller
               'https://www.recaptcha.net/recaptcha/api/siteverify',
               ['form_params'=>
                   [
-                    // 'secret'=> '6LeFKfYUAAAAABtTFzPon_8pinsPsevCSFyePD8k',
-                      'secret'=> '6LdshPcUAAAAACaoDOvGo7ncKgVazbyKoDlPi43T',
+                    'secret'=> '6LeFKfYUAAAAABtTFzPon_8pinsPsevCSFyePD8k',
+                    //   'secret'=> '6LdshPcUAAAAACaoDOvGo7ncKgVazbyKoDlPi43T',
                       'response'=>$request->keyresponseCap
                    ]
               ]
