580fa2c06a80a7ab8af00cf58f7a2b507710a793
chaisa097 <chaisa097@gmail.com>
2022-10-12 15:09:06 +0700

    update config

--- dependencies/resources/views/front-end/configurableproduct.blade.php
+++ dependencies/resources/views/front-end/configurableproduct.blade.php
@@ -2134,8 +2134,8 @@ function selectCountry(){
             '<html><head>'+
 			'<link rel="stylesheet" href="{{asset('/frontend-asset/css/bootstrap.min.css')}}" >'+
 			'<link rel="stylesheet" href="{{asset('/frontend-asset/css/font.css')}}">'+
-			// '<link rel="stylesheet" href="{{asset('/frontend-asset/css/embepdf.css')}}">'+
-		    '<link rel="stylesheet" href="{{asset('/frontend-asset/css/header-front.css')}}">'+
+			'<link rel="stylesheet" href="{{asset('/frontend-asset/css/embepdf.css')}}">'+
+		 '<link rel="stylesheet" href="{{asset('/frontend-asset/css/header-front.css')}}">'+
 			'<link rel="stylesheet" href="{{asset('/frontend-asset/css/container.css')}}">'+
 			'<link rel="stylesheet" href="{{asset('/frontend-asset/css/home.css')}}">'+
 			'<link rel="stylesheet" href="{{asset('/frontend-asset/css/product.css')}}">'+
