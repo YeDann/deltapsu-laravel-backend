28c392a18bd0eb4930cf910f267a1bee87a6c8a8
chaisa097 <chaisa097@gmail.com>
2023-12-08 10:05:56 +0700

    update

--- dependencies/routes/web.php
+++ dependencies/routes/web.php
@@ -34,7 +34,7 @@ Route::group([
 Route::get('/{page?}','FrontendController@index')->name('index');
 Route::get('/download/{doc?}','FrontendController@oldDoc')->name('downloadDocData');
 Route::get('/download/resources-leaflets/{doc?}','FrontendController@downloadoldLeaflets')->name('downloadoldLeaflets');
-Route::get('/file_doc/marketing_resources/{doc?}','FrontendController@checkpermission')->name('checkpermission');
+Route::get('/file_doc_2/marketing_resources/{doc?}','FrontendController@checkpermission')->name('checkpermission');
 
 Route::post('partnerLoginDoc','FrontendController@partnerLoginDoc')->name('partnerLoginDoc');
 Route::get('/loginDocPartner/{doc?}','FrontendController@loginDocPartner')->name('loginDocPartner');
