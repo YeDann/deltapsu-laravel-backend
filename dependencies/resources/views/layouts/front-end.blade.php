19f993c5de337862e104e47c4c3c150d25a832be
chaisa097 <chaisa097@gmail.com>
2023-12-12 18:15:41 +0700

    up mix css

--- dependencies/resources/views/layouts/front-end.blade.php
+++ dependencies/resources/views/layouts/front-end.blade.php
@@ -273,7 +273,10 @@ $langch = str_replace('_', '-', app()->getLocale());
   @include('layouts.header-front')
   @yield('container')
   @include('layouts.footer')
-  <script type="text/javascript" src="{{asset('/frontend-asset/js/nouislider.min.js')}}"></script>
+
+  <script type="text/javascript" src="{{asset('/frontend-asset/js/all.js')}}"></script>
+
+  {{-- <script type="text/javascript" src="{{asset('/frontend-asset/js/nouislider.min.js')}}"></script>
   <script type="text/javascript" src="{{asset('/frontend-asset/js/datatables.min.js')}}"></script>
   <script type="text/javascript" src="{{asset('/frontend-asset/js/popper.min.js')}}"></script>
   <script type="text/javascript" src="{{asset('/frontend-asset/js/bootstrap.min.js')}}"></script>
@@ -284,7 +287,7 @@ $langch = str_replace('_', '-', app()->getLocale());
   <script type="text/javascript" src="{{asset('/frontend-asset/js/owl.carousel.min.js')}}"></script>
   <script type="text/javascript" src="{{asset('/frontend-asset/js/slick.min.js')}}"></script>
   <script type="text/javascript" src="{{asset('/frontend-asset/js/zabuto_calendar.min.js')}}"></script>
-  <script type="text/javascript" src="{{asset('/frontend-asset/js/mb5.js')}}"></script>
+  <script type="text/javascript" src="{{asset('/frontend-asset/js/mb5.js')}}"></script> --}}
   <script src="https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit" async defer>
   </script>
   <script>
