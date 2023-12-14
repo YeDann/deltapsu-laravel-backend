2b07dd835a75a79af6311398088a07bb89f07365
chaisa097 <chaisa097@gmail.com>
2023-12-14 15:44:13 +0700

    update permission file

--- dependencies/app/Http/Controllers/FrontendController.php
+++ dependencies/app/Http/Controllers/FrontendController.php
@@ -4817,19 +4817,20 @@ class FrontendController extends Controller
                 ->select('mr.*' ,'mrt.*')
                 ->first();
     
-        if(isset($margeting_public)){
-           return response()->download($path);
-        }
-        else if(isset($sales_kits)){
-            return redirect()->route('loginDocPartner',$doc );                 
+        // if(isset($margeting_public)){
+        //    return response()->download($path);
+        // }
+        // else if(isset($sales_kits)){
+        //     return redirect()->route('loginDocPartner',$doc );                 
           
-        }
-        else if(isset($margeting)){
-            return redirect()->route('loginDocPartner',$doc );  
-        }
-        else{
-            return abort(404);
-        }
+        // }
+        // else if(isset($margeting)){
+        //     return redirect()->route('loginDocPartner',$doc );  
+        // }
+        // else{
+        //     return abort(404);
+        // }
+        return redirect()->route('loginDocPartner',$doc );  
 
       }
 
