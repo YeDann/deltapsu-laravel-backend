a158e41d3db2a97e3689b9811a698b16df60ab80
chaisa097 <chaisa097@gmail.com>
2024-01-11 10:51:32 +0700

    fix lang cookie wow

--- dependencies/resources/views/layouts/front-end.blade.php
+++ dependencies/resources/views/layouts/front-end.blade.php
@@ -15,7 +15,7 @@ $langch = str_replace('_', '-', app()->getLocale());
 
 ?>
 
-<html lang="{{$lanngCookie}}">
+<html html_lang="{{ str_replace('_', '-', app()->getLocale()) }}" lang="{{$lanngCookie}}">
 
 <head>
   <meta charset="utf-8">
