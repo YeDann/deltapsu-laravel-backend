6e52d47252d1f60423eae523ed509373bb42f01b
chaisa097 <chaisa097@gmail.com>
2024-01-11 11:14:01 +0700

    update lang

--- dependencies/resources/views/layouts/front-end.blade.php
+++ dependencies/resources/views/layouts/front-end.blade.php
@@ -711,15 +711,15 @@ if (!Array.prototype.findIndex) {
         }
         function setlocaltion(lang ,link){
           // console.log(lang);
-          if(lang == 'jp'){
-            window.cwcCookieBanner.setLang('ja') 
-          }else if(lang == 'cn'){
-            window.cwcCookieBanner.setLang('zh') 
-          }else if(lang == 'tw'){
-            window.cwcCookieBanner.setLang('zh') 
-          }else{
-            window.cwcCookieBanner.setLang('en') 
-          }
+          // if(lang == 'jp'){
+          //   window.cwcCookieBanner.setLang('ja') 
+          // }else if(lang == 'cn'){
+          //   window.cwcCookieBanner.setLang('zh') 
+          // }else if(lang == 'tw'){
+          //   window.cwcCookieBanner.setLang('zh') 
+          // }else{
+          //   window.cwcCookieBanner.setLang('en') 
+          // }
           $.ajax({
            url: "{{route('setlocaltion')}}",
            data: {
