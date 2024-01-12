cf78cd91d2cf60ab03b26ad3658ad783579af65f
chaisa097 <chaisa097@gmail.com>
2024-01-12 15:29:53 +0700

    Update

--- dependencies/resources/views/layouts/front-end.blade.php
+++ dependencies/resources/views/layouts/front-end.blade.php
@@ -254,10 +254,9 @@ $langch = str_replace('_', '-', app()->getLocale());
   </style>
 
   <!-- Cookie Consent by https://www.cookiewow.com -->
-  <!-- Cookie Consent by https://www.cookiewow.com -->
   <script type="text/javascript" src="https://cookiecdn.com/cwc.js"></script>
-  <script id="cookieWow" type="text/javascript" src="https://cookiecdn.com/configs/i7aRCyw6TPKrRYdtQqZzuN3Y"
-    data-cwcid="i7aRCyw6TPKrRYdtQqZzuN3Y"></script>
+  <script id="cookieWow" type="text/javascript" src="https://cookiecdn.com/configs/QCpdMWAZzK3aPN8JBDMiPWQN"
+    data-cwcid="QCpdMWAZzK3aPN8JBDMiPWQN"></script>
 
   <!-- Fonts -->
   <!-- Styles -->
