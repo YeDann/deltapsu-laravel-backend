7f2564484187cd693be3d487ed2509fe71fa1814
chaisa097 <chaisa097@gmail.com>
2024-02-16 10:27:34 +0700

    update

--- dependencies/app/Http/Controllers/FrontendController.php
+++ dependencies/app/Http/Controllers/FrontendController.php
@@ -3818,7 +3818,7 @@ class FrontendController extends Controller
             'https://www.recaptcha.net/recaptcha/api/siteverify',
             ['form_params'=>
                 [
-                     //'secret'=> '6LfGGV0pAAAAAMYsIjmPwbpfkta9cE_2YOguoR5T',
+                     'secret'=> '6LfGGV0pAAAAAMYsIjmPwbpfkta9cE_2YOguoR5T',
                       //'secret'=> '6LdshPcUAAAAACaoDOvGo7ncKgVazbyKoDlPi43T',
                       'secret'=> '6LfGGV0pAAAAAMYsIjmPwbpfkta9cE_2YOguoR5T',
                     'response'=>$request->keyrecap
@@ -4088,8 +4088,9 @@ class FrontendController extends Controller
               'https://www.recaptcha.net/recaptcha/api/siteverify',
               ['form_params'=>
                   [
-                    'secret'=> '6LeFKfYUAAAAABtTFzPon_8pinsPsevCSFyePD8k',
+                    //'secret'=> '6LeFKfYUAAAAABtTFzPon_8pinsPsevCSFyePD8k',
                     //   'secret'=> '6LdshPcUAAAAACaoDOvGo7ncKgVazbyKoDlPi43T',
+                    'secret'=> '6LfGGV0pAAAAAMYsIjmPwbpfkta9cE_2YOguoR5T',
                       'response'=>$request->keyresponseCap
                    ]
               ]
