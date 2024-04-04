d09077f1baad180af68cdf780e37e016b685d5f3
chaisa097 <chaisa097@gmail.com>
2024-04-04 15:00:06 +0700

    fix text Get Advice

--- dependencies/resources/views/feedbackEmail/contactFeebackform.blade.php
+++ dependencies/resources/views/feedbackEmail/contactFeebackform.blade.php
@@ -1,14 +1,13 @@
 @extends('layouts.admin')
 @section('style')
 <style>
-
     .Absolute-Center {
         position: relative;
-    left: 50%;
-    top: 50%;
-    transform: translate(-50%, -10%);
-   }
-}
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -10%);
+    }
+    }
 </style>
 @endsection
 @section('content')
@@ -44,88 +43,96 @@
     <div class="block block-rounded block-bordered">
         <div class="block-header block-header-default">
             <div class="Absolute-Center">
-                <select onchange="getFiler()" class="js-select2 form-control" id="pro_categories" name="categories_doc" data-placeholder="Filter By.." required>
+                <select onchange="getFiler()" class="js-select2 form-control" id="pro_categories" name="categories_doc"
+                    data-placeholder="Filter By.." required>
                     <option></option>
-                    <option value="0" {{isset($selecValue) &&  $selecValue == 'All' ? 'selected':'' }} >All</option>
-                    <option value="Sale Enquiries" {{isset($selecValue) &&  $selecValue == 'Sale Enquiries' ? 'selected':'' }} >Sale Enquiries</option>
-                    <option value="Products and Service Support" {{isset($selecValue) &&  $selecValue == 'Products and Service Support' ? 'selected':'' }}>Products and Service Support</option>
-                    <option value="General Comments" {{isset($selecValue) &&  $selecValue == 'General Comments' ? 'selected':'' }} >General Comments</option> 
+                    <option value="0" {{isset($selecValue) && $selecValue=='All' ? 'selected' :'' }}>All</option>
+                    <option value="Sale Enquiries" {{isset($selecValue) && $selecValue=='Sale Enquiries' ? 'selected'
+                        :'' }}>Sale Enquiries</option>
+                    <option value="Products and Service Support" {{isset($selecValue) &&
+                        $selecValue=='Products and Service Support' ? 'selected' :'' }}>Products and Service Support
+                    </option>
+                    <option value="Get Advice" {{isset($selecValue) && $selecValue=='Get Advice' ? 'selected' :'' }}>Get
+                        Advice</option>
                 </select>
             </div>
             <h3 class="block-title">
             </h3>
             <div class="block-options">
                 <div class="block-options-item">
-                
+
                 </div>
                 <div class="block-options-item">
-                  <a href="{{route('exportfeedbackFrom',$selecValue)}}" class="btn btn-outline-primary">Export Data</a> 
+                    <a href="{{route('exportfeedbackFrom',$selecValue)}}" class="btn btn-outline-primary">Export
+                        Data</a>
                 </div>
             </div>
         </div>
-        <div class="block-content block-content-full" >
+        <div class="block-content block-content-full">
             <div class="table-responsive">
-            <table id="dtBasicExample"  class="table table-striped table-bordered" cellspacing="0" width="100%">
-                <thead>
-                    <tr>
-                        <th class="th-sm">No.</th>
-                        <th class="th-sm">Subject</th>
-                        <th class="th-sm">Ticket No</th>
-                        <th class="th-sm">Email</th>
-                        <th class="th-sm">Name</th>
-                        <th class="th-sm">Country</th>
-                        <th class="th-sm">Type</th>
-                        <th class="th-sm">Model</th>
-                        <th class="th-sm">Tel</th>
-                        <th class="th-sm">Created_at</th>
-                        <th class="th-sm">Config file</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @if(isset($contactemail) and !empty($contactemail))
-                    @foreach ($contactemail as $item)
-                    <td class="text-center">{{$loop->iteration}}</td>
-                    <td class="d-none d-sm-table-cell">{{$item->subject}}</td>
-                    <td class="d-none d-sm-table-cell">{{$item->ticket_id}}</td>
-                    <td class="d-none d-sm-table-cell">{{$item->email}}</td>
-                    <td class="d-none d-sm-table-cell">{{$item->name}}</td>
-                    <td class="d-none d-sm-table-cell">{{$item->country}}</td>
-                    <td class="d-none d-sm-table-cell">{{$item->type_name}}</td>
-                    <td class="d-none d-sm-table-cell">{{$item->model_name}}</td>
-                    <td class="d-none d-sm-table-cell">{{$item->tel}}</td>
-                    <td class="d-none d-sm-table-cell">{{$item->created_at}}</td>
-                    <td class="text-center">
-                     @if(isset($item->file))
-                    <a href="{{config('app.url')}}/config_history/{{$item->file}}"   target="_blank" class="btn btn-outline-primary">View</a>
-                    @else 
-                     No file
-                      @endif
-                    </td>
-                    </tr>
-                    @endforeach
-                    @endif
-                </tbody>
-            </table>
+                <table id="dtBasicExample" class="table table-striped table-bordered" cellspacing="0" width="100%">
+                    <thead>
+                        <tr>
+                            <th class="th-sm">No.</th>
+                            <th class="th-sm">Subject</th>
+                            <th class="th-sm">Ticket No</th>
+                            <th class="th-sm">Email</th>
+                            <th class="th-sm">Name</th>
+                            <th class="th-sm">Country</th>
+                            <th class="th-sm">Type</th>
+                            <th class="th-sm">Model</th>
+                            <th class="th-sm">Tel</th>
+                            <th class="th-sm">Created_at</th>
+                            <th class="th-sm">Config file</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @if(isset($contactemail) and !empty($contactemail))
+                        @foreach ($contactemail as $item)
+                        <td class="text-center">{{$loop->iteration}}</td>
+                        <td class="d-none d-sm-table-cell">{{$item->subject}}</td>
+                        <td class="d-none d-sm-table-cell">{{$item->ticket_id}}</td>
+                        <td class="d-none d-sm-table-cell">{{$item->email}}</td>
+                        <td class="d-none d-sm-table-cell">{{$item->name}}</td>
+                        <td class="d-none d-sm-table-cell">{{$item->country}}</td>
+                        <td class="d-none d-sm-table-cell">{{$item->type_name}}</td>
+                        <td class="d-none d-sm-table-cell">{{$item->model_name}}</td>
+                        <td class="d-none d-sm-table-cell">{{$item->tel}}</td>
+                        <td class="d-none d-sm-table-cell">{{$item->created_at}}</td>
+                        <td class="text-center">
+                            @if(isset($item->file))
+                            <a href="{{config('app.url')}}/config_history/{{$item->file}}" target="_blank"
+                                class="btn btn-outline-primary">View</a>
+                            @else
+                            No file
+                            @endif
+                        </td>
+                        </tr>
+                        @endforeach
+                        @endif
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
 </div>
- <!-- Vertically Centered Block Modal -->
- <div class="modal" id="modal-block-vcenter" tabindex="-1" role="dialog" aria-labelledby="modal-block-vcenter" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered" role="document">
-            <div class="modal-content">
-                <div class="block block-themed block-transparent mb-0">
-                    <div class="block-header bg-danger">
-                        <h3 class="block-title">!! Warning </h3>
-                        <div class="block-options">
-                            <button type="button" class="btn-block-option" data-dismiss="modal" aria-label="Close">
-                                <i class="fa fa-fw fa-times"></i>
-                            </button>
-                        </div>
+<!-- Vertically Centered Block Modal -->
+<div class="modal" id="modal-block-vcenter" tabindex="-1" role="dialog" aria-labelledby="modal-block-vcenter"
+    aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="block block-themed block-transparent mb-0">
+                <div class="block-header bg-danger">
+                    <h3 class="block-title">!! Warning </h3>
+                    <div class="block-options">
+                        <button type="button" class="btn-block-option" data-dismiss="modal" aria-label="Close">
+                            <i class="fa fa-fw fa-times"></i>
+                        </button>
                     </div>
-                    <form action="{{route('deleteConfigProduct')}}" method="POST" >
-                        {{csrf_field()}}
-                      
+                </div>
+                <form action="{{route('deleteConfigProduct')}}" method="POST">
+                    {{csrf_field()}}
+
                     <div class="block-content">
                         <input type="hidden" name="itemId" id="itemId">
                         <p>Data will be lost?</p>
@@ -134,18 +141,18 @@
                         <button type="button" class="btn  btn-light" data-dismiss="modal">Close</button>
                         <button type="submit" class="btn btn-success">OK</button>
                     </div>
-                    </form>
-                </div>
+                </form>
             </div>
         </div>
     </div>
-    <!-- END Vertically Centered Block Modal -->
+</div>
+<!-- END Vertically Centered Block Modal -->
 
 
 @endsection
 @section('js')
 <script>
-   function getFiler(){
+    function getFiler(){
         var value =  $('#pro_categories').val();
         if(value == 0){
             window.location = '{{route('feedbackform')}}/All';
