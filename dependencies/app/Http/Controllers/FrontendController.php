f4936a1c6045941e4dca3e6ff5397c81b88ea834
chaisa097 <chaisa097@gmail.com>
2024-05-13 14:59:02 +0700

    update

--- dependencies/app/Http/Controllers/FrontendController.php
+++ dependencies/app/Http/Controllers/FrontendController.php
@@ -71,6 +71,15 @@ class FrontendController extends Controller
     ->where('sct.local',  $lang)
     ->orderBy('chmp.order_seq', 'asc')
     ->get());
+
+    view()->share('navcategories4',  DB::table('categories_has_main_pro as chmp')
+    ->join('sub_pro_categories as sc', 'chmp.cate_id', '=', 'sc.sub_pro_id')
+    ->join('sub_pro_categories_translation as sct', 'sct.sub_pro_id', '=', 'sc.sub_pro_id')
+    ->select('sc.*', 'sct.*' ,'chmp.*')
+    ->where('sct.local',  $lang)
+    ->where('chmp.main_cateid', 4)
+    ->orderBy('chmp.order_seq', 'asc')
+    ->get());
    
     view()->share('navapplication', DB::table('application as ap')
     ->join('application_translation as apt','ap.id','=','apt.app_id')
