5826463820e535140a86a6db685b9e106c4ed45d
chaisa097 <chaisa097@gmail.com>
2024-08-19 13:49:53 +0700

    up CSRF token

--- dependencies/app/Http/Middleware/VerifyCsrfToken.php
+++ dependencies/app/Http/Middleware/VerifyCsrfToken.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
+use Symfony\Component\HttpFoundation\Cookie;
 
 class VerifyCsrfToken extends Middleware
 {
@@ -21,25 +22,25 @@ class VerifyCsrfToken extends Middleware
     protected $except = [
         //
     ];
-    // protected function addCookieToResponse($request, $response)
-    //         {
-    //             $config = config('session');
+    protected function addCookieToResponse($request, $response)
+            {
+                $config = config('session');
 
-    //             $response->headers->setCookie(
-    //                 new Cookie(
-    //                     'XSRF-TOKEN',
-    //                     $request->session()->token(),
-    //                     $this->availableAt(60 * $config['lifetime']),
-    //                     $config['path'],
-    //                     $config['domain'],
-    //                     $config['secure'],
-    //                     true,  // Set HttpOnly to true
-    //                     false,
-    //                     false,
-    //                     $config['same_site'] ?? null
-    //                 )
-    //             );
+                $response->headers->setCookie(
+                    new Cookie(
+                        'XSRF-TOKEN',
+                        $request->session()->token(),
+                        $this->availableAt(60 * $config['lifetime']),
+                        $config['path'],
+                        $config['domain'],
+                        $config['secure'],
+                        true,  // Set HttpOnly to true
+                        false,
+                        false,
+                        $config['same_site'] ?? null
+                    )
+                );
 
-    //             return $response;
-    //         }
+                return $response;
+            }
 }
