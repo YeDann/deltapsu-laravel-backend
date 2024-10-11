9050cef4e4a46597077b6b4b04a25d40202fe913
chaisa097 <chaisa097@gmail.com>
2024-10-11 15:01:55 +0700

    Verify Lang php

---
+++ dependencies/app/Http/Middleware/VerifyLang.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Middleware;
+use DB;
+use App;
+use Closure;
+class VerifyLang
+
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $lang = App::getLocale();
+        if($lang){
+        // Check if the language exists in the database
+           $check_lang = DB::table('language')
+                    ->where('language.name','=', $lang)
+                    ->where('language.status', '=', 1)
+                    ->first();
+                if (!$check_lang) {
+                    App::setLocale('en');
+                    return redirect('/en/404');
+                }
+        }
+        return $next($request);
+    }
+}
