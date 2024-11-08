c64e4d2e18be4fcd5dca5be4de31895bb67a81f7
chaisa097 <chaisa097@gmail.com>
2024-11-08 09:55:57 +0700

    Revert "update web"
    This reverts commit 82ff6fca0663ad9d6783bf0173cfac6b21677d98.

--- dependencies/routes/web.php
+++ dependencies/routes/web.php
@@ -29,7 +29,7 @@ Route::get('/products/download/{lang?}/{cate_name?}/{modelname?}','FrontendContr
 
 Route::group([
      'prefix' => LaravelLocalization::setLocale(),
-    'middleware' => ['localeSessionRedirect', 'localizationRedirect', 'localeViewPath' ,'HtmlMinifier','verifyLang']
+    'middleware' => ['localeSessionRedirect', 'localizationRedirect', 'localeViewPath' ,'HtmlMinifier']
         ], function() {
 Route::get('/{page?}','FrontendController@index')->name('index');
 Route::get('/download/{doc?}','FrontendController@oldDoc')->name('downloadDocData');
