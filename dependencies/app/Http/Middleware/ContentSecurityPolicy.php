0d055e572095843dd68ea521d467f7cb8ab7f979
Suphakornpk <aum@degitobangkok.com>
2024-11-22 14:42:27 +0700

    Update ContentSecurityPolicy.php

--- dependencies/app/Http/Middleware/ContentSecurityPolicy.php
+++ dependencies/app/Http/Middleware/ContentSecurityPolicy.php
@@ -20,7 +20,7 @@ class ContentSecurityPolicy
             "style-src 'self' 'unsafe-inline';",
             "font-src 'self';",
             "media-src 'none';",
-            "frame-src 'self' https://www.youtube.com https://www.googletagmanager.com;",
+            "frame-src 'self' https://www.youtube.com https://www.googletagmanager.com https://www.google.com;",
             "connect-src 'self' https://104.42.107.127 https://www.google.com https://www.googletagmanager.com https://www.gstatic.com https://www.google-analytics.com https://pagead2.googlesyndication.com;",
         ];
 
