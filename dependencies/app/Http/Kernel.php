cab867e81a4e435ecd677abed2d903d4c259fdd3
Suphakornpk <aum@degitobangkok.com>
2024-11-22 13:24:06 +0700

    Update Kernel.php

--- dependencies/app/Http/Kernel.php
+++ dependencies/app/Http/Kernel.php
@@ -20,7 +20,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
         \App\Http\Middleware\ModifyRedirects::class,
-        \App\Http\Middleware\ContentSecurityPolicy::class,
+        // \App\Http\Middleware\ContentSecurityPolicy::class,
         //\RenatoMarinho\LaravelPageSpeed\Middleware\InlineCss::class,
     //     \RenatoMarinho\LaravelPageSpeed\Middleware\ElideAttributes::class,
     //     \RenatoMarinho\LaravelPageSpeed\Middleware\InsertDNSPrefetch::class,
