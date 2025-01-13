60f126935f5b6aba8bc36ddeaadcec3d710f6f71
chaisa097 <chaisa097@gmail.com>
2025-01-13 14:36:30 +0700

    update data placeholder

--- dependencies/resources/views/product_doc/create_doc.blade.php
+++ dependencies/resources/views/product_doc/create_doc.blade.php
@@ -78,7 +78,7 @@
                         <div class="form-group">
                             <label for="example-select"><span class="req-fed">*</span> Multiple Select Products</label>
                             <select class="js-select2 form-control" id="ModelIdNotSelectIncate" name="product[]"
-                                style="width: 100%;" data-placeholder="Choose many.." multiple disabled>
+                                style="width: 100%;" data-placeholder="waiting.." multiple disabled>
                                 <option></option>
                                 <!-- Required for data-placeholder attribute to work with Select2 plugin -->
                                 {{-- @foreach ($products as $item)
@@ -141,7 +141,9 @@
                         .pro_code + '</option>';
                 }
                 $("select#ModelIdNotSelectIncate").html(options);
-                $("select#ModelIdNotSelectIncate").html(options).prop('disabled', false);
+                $("select#ModelIdNotSelectIncate").html(options)
+                .prop('disabled', false)
+                .attr('data-placeholder', 'Choose many..');
             }
 
         });
