da5f76185f19b86043d0e411f4dd396ef6b2383e
chaisa097 <chaisa097@gmail.com>
2025-03-13 17:02:16 +0700

    update

--- dependencies/resources/views/layouts/header-front.blade.php
+++ dependencies/resources/views/layouts/header-front.blade.php
@@ -1147,7 +1147,9 @@ function slugifyHead($text)
                     $Sales_Inquiry = $staticContent['Sales_Inquiry'];
                  }
                 ?>
-                @if(App::getLocale() == "cn" || App::getLocale() == "tw" && $Sales_Inquiry != 'Sales Inquiry')
+                @if(
+                App::getLocale() == "cn" & strtolower(str_replace(' ', '', $Sales_Inquiry)) != 'Sales Inquiry'
+                || App::getLocale() == "tw" && strtolower(str_replace(' ', '', $Sales_Inquiry)) != 'Sales Inquiry')
                 <span class="vertical-text">
                     {{$Sales_Inquiry}}
                 </span>
