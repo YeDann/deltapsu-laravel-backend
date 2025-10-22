be4a1c83c67abf40e7e346297251202a24368d7a
chaisa097 <chaisa097@gmail.com>
2025-10-22 17:14:55 +0700

    update data

--- dependencies/app/Http/Middleware/ContentSecurityPolicy.php
+++ dependencies/app/Http/Middleware/ContentSecurityPolicy.php
@@ -11,22 +11,19 @@ class ContentSecurityPolicy
     {
         $response = $next($request);
 
-        //UAT
-        $cspDirectives = [
-            "default-src 'self';",
-            "script-src 'self' 'unsafe-inline' https://cookiecdn.com https://code.jquery.com https://www.googletagmanager.com https://cdnjs.cloudflare.com https://cdn.jsdelivr.net https://www.google.com https://www.gstatic.com https://snap.licdn.com;",
-            "img-src 'self' data: https://px.ads.linkedin.com;",
-            "object-src 'none';",
-            "style-src 'self' 'unsafe-inline';",
-            "font-src 'self';",
-            "media-src 'none';",
-            "frame-src 'self' https://www.youtube.com https://www.googletagmanager.com https://www.google.com;",
-            "connect-src 'self' https://www.google.com https://www.googletagmanager.com https://www.gstatic.com https://www.google-analytics.com https://pagead2.googlesyndication.com https://px.ads.linkedin.com;",
+       $cspDirectives = [
+            "default-src 'self'",
+            "script-src 'self' 'unsafe-inline' https://cookiecdn.com https://code.jquery.com https://www.googletagmanager.com https://cdnjs.cloudflare.com https://cdn.jsdelivr.net https://www.google.com https://www.gstatic.com https://snap.licdn.com",
+            "img-src 'self' data: https://px.ads.linkedin.com https://cookiecdn.com",
+            "object-src 'none'",
+            "style-src 'self' 'unsafe-inline'",
+            "font-src 'self'",
+            "media-src 'none'",
+            "frame-src 'self' https://www.youtube.com https://www.googletagmanager.com https://www.google.com",
+            "connect-src 'self' https://api.cookiewow.com https://www.google.com https://www.googletagmanager.com https://www.gstatic.com https://www.google-analytics.com https://pagead2.googlesyndication.com https://px.ads.linkedin.com https://www.youtube.com https://i.ytimg.com"
         ];
 
         $response->headers->set('Content-Security-Policy', implode('; ', $cspDirectives));
-        $response->headers->set('X-Content-Security-Policy', implode('; ', $cspDirectives));
-        $response->headers->set('X-WebKit-CSP', implode('; ', $cspDirectives));
         $response->headers->set('Cache-Control', 'no-store');
 
 
