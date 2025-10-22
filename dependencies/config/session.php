4c18626e94b7f39027b111fb995b26319e9aae90
chaisa097 <chaisa097@gmail.com>
2025-10-22 17:25:22 +0700

    update data

--- dependencies/config/session.php
+++ dependencies/config/session.php
@@ -31,12 +31,12 @@ return [
     |
     */
 
-    //'lifetime' => env('SESSION_LIFETIME', 2),
+    'lifetime' => env('SESSION_LIFETIME', 2),
 
-    //'expire_on_close' => false,
-    'lifetime' =>  0,
+    'expire_on_close' => false,
+    //'lifetime' =>  0,
 
-    'expire_on_close' => true,
+    //'expire_on_close' => true,
 
     /*
     |--------------------------------------------------------------------------
