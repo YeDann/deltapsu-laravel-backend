1fc5108d2789ef9faee1c76b299035de51c3e6f2
chaisa097 <chaisa097@gmail.com>
2025-10-23 19:31:23 +0700

    update

--- dependencies/app/Http/Middleware/ContentSecurityPolicy.php
+++ dependencies/app/Http/Middleware/ContentSecurityPolicy.php
@@ -13,14 +13,14 @@ class ContentSecurityPolicy
 
        $cspDirectives = [
             "default-src 'self'",
-            "script-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net https://cookiecdn.com https://code.jquery.com https://www.googletagmanager.com https://analytics.google.com https://cdnjs.cloudflare.com https://cdn.jsdelivr.net https://www.google.com https://www.gstatic.com https://snap.licdn.com",
+            "script-src 'self' 'unsafe-inline' https://cookiecdn.com https://code.jquery.com https://www.googletagmanager.com https://analytics.google.com https://cdnjs.cloudflare.com https://cdn.jsdelivr.net https://www.google.com https://www.gstatic.com https://snap.licdn.com",
             "img-src 'self' data: https://px.ads.linkedin.com https://cookiecdn.com",
             "object-src 'none'",
-            "style-src 'self' 'unsafe-inline'",
+            "style-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net",
             "font-src 'self'",
             "media-src 'none'",
             "frame-src 'self' https://www.youtube.com https://www.googletagmanager.com https://www.google.com https://analytics.google.com",
-            "connect-src 'self' https://api.cookiewow.com https://cdn.jsdelivr.net https://www.google.com https://www.googletagmanager.com https://www.gstatic.com https://analytics.google.com https://www.google-analytics.com https://pagead2.googlesyndication.com https://px.ads.linkedin.com https://www.youtube.com https://i.ytimg.com"
+            "connect-src 'self' https://api.cookiewow.com https://www.google.com https://www.googletagmanager.com https://www.gstatic.com https://analytics.google.com https://www.google-analytics.com https://pagead2.googlesyndication.com https://px.ads.linkedin.com https://www.youtube.com https://i.ytimg.com"
         ];
 
         $response->headers->set('Content-Security-Policy', implode('; ', $cspDirectives));
