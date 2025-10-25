e816b77e6775708effe33e58e7091cfbcaf5a65c
chaisa097 <chaisa097@gmail.com>
2025-10-25 11:44:41 +0700

    update data

--- dependencies/resources/views/front-end/configurableproduct.blade.php
+++ dependencies/resources/views/front-end/configurableproduct.blade.php
@@ -1118,9 +1118,7 @@
 	};
 	var onloadCallback = function() {
 	  grecaptcha.render('recap_vertify', {
-	//    'sitekey' : '6LdshPcUAAAAACIioRg3pa05GCUYQ9S0hVLv-4zv',
-         'sitekey' : '6LeFKfYUAAAAAL-q5mHlmjUTPQ-LvlDjNtev9QhA',
-								//'sitekey' : '6LcPwHQpAAAAAL5BjEcFskLuvXnrUP9aNeeCv_9R',
+        'sitekey' : '{{config('app.recapcha_site_key')}}',
 		'callback' : verifyCallback,
 		'theme' : 'light'
 	  });
