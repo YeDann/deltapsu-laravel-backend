8c827a5844479328191d3d07db6c449c754b6b51
chaisa097 <chaisa097@gmail.com>
2025-11-05 00:51:02 +0700

    update

--- dependencies/app/Http/Middleware/ContentSecurityPolicy.php
+++ dependencies/app/Http/Middleware/ContentSecurityPolicy.php
@@ -20,7 +20,7 @@ class ContentSecurityPolicy
             "object-src 'none'",
             "media-src 'self' https://*.youtube.com https://*.ytimg.com",
             "frame-src 'self' https://*.youtube.com https://*.google.com https://*.googletagmanager.com",
-            "connect-src 'self' https://*.google.com https://*.googletagmanager.com https://*.gstatic.com https://*.google-analytics.com https://*.doubleclick.net https://*.youtube.com https://*.ytimg.com https://api.cookiewow.com https://*.linkedin.com"
+            "connect-src 'self' https://*.google.com https://*.googletagmanager.com https://*.gstatic.com https://*.google-analytics.com https://*.doubleclick.net https://*.youtube.com https://*.ytimg.com https://api.cookiewow.com https://*.linkedin.com https://*.googlesyndication.com"
         ];
 
         $response->headers->set('Content-Security-Policy', implode('; ', $cspDirectives));
