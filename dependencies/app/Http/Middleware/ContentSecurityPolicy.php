ff6adc1d46f22032fae86bf46fb85437582fb5c1
chaisa097 <chaisa097@gmail.com>
2025-11-04 17:21:17 +0700

    update

--- dependencies/app/Http/Middleware/ContentSecurityPolicy.php
+++ dependencies/app/Http/Middleware/ContentSecurityPolicy.php
@@ -13,13 +13,13 @@ class ContentSecurityPolicy
 
        $cspDirectives = [
             "default-src 'self'",
-            "script-src 'self' 'unsafe-inline' https://cookiecdn.com https://code.jquery.com https://www.googletagmanager.com https://analytics.google.com https://cdnjs.cloudflare.com https://cdn.jsdelivr.net https://www.google.com https://www.gstatic.com https://snap.licdn.com",
-            "img-src 'self' data: https://px.ads.linkedin.com https://cookiecdn.com https://deltapsu.com https://www.deltapsu.com https://psu.deltaww.com",
+            "script-src 'self' 'unsafe-inline' https://cookiecdn.com https://code.jquery.com https://www.googletagmanager.com https://analytics.google.com https://cdnjs.cloudflare.com https://cdn.jsdelivr.net https://www.google.com https://www.gstatic.com https://snap.licdn.com https://www.youtube.com https://s.ytimg.com",
+            "img-src 'self' data: https://px.ads.linkedin.com https://cookiecdn.com https://deltapsu.com https://www.deltapsu.com https://psu.deltaww.com https://i.ytimg.com",
             "object-src 'none'",
             "style-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net",
             "font-src 'self'",
-            "media-src 'none'",
-            "frame-src 'self' https://www.youtube.com https://www.googletagmanager.com https://www.google.com https://analytics.google.com",
+            "media-src 'self' https://www.youtube.com https://img.youtube.com https://i.ytimg.com https://www.youtube-nocookie.com",
+            "frame-src 'self' https://www.youtube.com https://www.googletagmanager.com https://www.google.com https://analytics.google.com https://www.youtube-nocookie.com",
             "connect-src 'self' https://api.cookiewow.com https://www.google.com https://www.googletagmanager.com  https://www.gstatic.com https://analytics.google.com https://www.google-analytics.com https://pagead2.googlesyndication.com https://px.ads.linkedin.com https://www.youtube.com https://img.youtube.com https://stats.g.doubleclick.net https://i.ytimg.com"
         ];
 
