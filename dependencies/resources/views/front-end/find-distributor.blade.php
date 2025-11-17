bc772ae434e79989feb1039742370c51102f1072
dan <dan@twjoin.com>
2025-11-17 23:28:12 +0800

    feat: ace office add locale EC btn

--- dependencies/resources/views/front-end/find-distributor.blade.php
+++ dependencies/resources/views/front-end/find-distributor.blade.php
@@ -108,13 +108,13 @@
                                 {!!$office->content!!}
                             </div>
 
-                            @if($office->local == 'tw' && $office->id == 47)
                             {{-- 羅昇企業股份有限公司 加上電商網站按鈕 office->id = 47 --}}
-                            <a href="https://www.acepillar-ec.com/collections/delta" target="_blank">
-                                <button class="btn-subscribe mt-2">
-                                    電商網站
-                                </button>
-                            </a>
+                            @if ($office->id == 47)
+                                <a href="https://www.acepillar-ec.com/collections/delta" target="_blank">
+                                    <button class="btn-subscribe mt-2">
+                                        {{ App::getLocale() == 'tw' ? '電商網站' : 'Buy Now' }}
+                                    </button>
+                                </a>
                             @endif
 
                             <a href="https://www.google.com/maps/?q={{$office->lat}},{{$office->lon}}&sensor=true"
