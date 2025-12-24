70a3537b61ad1815f03319d0e07f5e09b01d0d73
dan <dan@twjoin.com>
2025-12-24 12:13:52 +0800

    feat:

---
+++ dependencies/app/helpers.php
@@ -0,0 +1,30 @@
+<?php
+
+if (!function_exists('slugifyHead')) {
+    function slugifyHead($text)
+    {
+        // replace non letter or digits by -
+        $text = preg_replace('~[^\\pL\d]+~u', '-', $text);
+
+        // trim
+        $text = trim($text, '-');
+
+        // transliterate
+        // if (function_exists('iconv'))
+        // {
+        //     $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+        // }
+
+        // lowercase
+        $text = strtolower($text);
+
+        // remove unwanted characters
+        $text = preg_replace('~[^-\w]+~', '', $text);
+        if (empty($text))
+        {
+            return 'n-a';
+        }
+
+        return $text;
+    }
+}
--- dependencies/resources/views/layouts/header-front.blade.php
+++ dependencies/resources/views/layouts/header-front.blade.php
@@ -329,33 +329,6 @@
         padding: 6px 3px;
     }
 </style>
-<?php
-function slugifyHead($text)
-            {
-            // replace non letter or digits by -
-            $text = preg_replace('~[^\\pL\d]+~u', '-', $text);
-
-            // trim
-            $text = trim($text, '-');
-
-            // transliterate
-            // if (function_exists('iconv'))
-            // {
-            //     $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
-            // }
-
-            // lowercase
-            $text = strtolower($text);
-
-            // remove unwanted characters
-            $text = preg_replace('~[^-\w]+~', '', $text);
-            if (empty($text))
-            {
-                return 'n-a';
-            }
-
-            return $text;
-            }
 
 ?>
 <div class="invisible-nav-minimize">
