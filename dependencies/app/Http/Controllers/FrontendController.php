0651c8d7d5ca5b84b6f267ef322a526275996e64
dan <dan@twjoin.com>
2026-01-05 18:58:40 +0800

    fix: remove gui download href

--- dependencies/app/Http/Controllers/FrontendController.php
+++ dependencies/app/Http/Controllers/FrontendController.php
@@ -4477,13 +4477,13 @@ class FrontendController extends Controller
                 $typeName = $this->validateInput($request->procateGui, 'text', true);
 
                 $subCategories = DB::table('sub_pro_categories as sp')
-            ->join('sub_pro_categories_translation as spt', 'spt.sub_pro_id', '=', 'sp.sub_pro_id')
-            ->where('spt.local', '=', $lang)
-            ->where('spt.name', '=', $typeName)
-            ->where('sp.status', '=', 1)
-            ->select('sp.sub_pro_id')
-            ->orderBy('sp.order_seq', 'asc')
-            ->first();
+                    ->join('sub_pro_categories_translation as spt', 'spt.sub_pro_id', '=', 'sp.sub_pro_id')
+                    ->where('spt.local', '=', $lang)
+                    ->where('spt.name', '=', $typeName)
+                    ->where('sp.status', '=', 1)
+                    ->select('sp.sub_pro_id')
+                    ->orderBy('sp.order_seq', 'asc')
+                    ->first();
 
                 $path = base_path('../upload/product_files/') . $filename;
                 $emailsend = [];
@@ -4508,11 +4508,11 @@ class FrontendController extends Controller
                 }
 
                 $emailSg1 = DB::table('email_notification as et')
-            ->select('et.*')
-            ->where('et.country', $country)
-            ->where('et.type', 1)
-            ->orderBy('et.country', 'asc')
-            ->first();
+                    ->select('et.*')
+                    ->where('et.country', $country)
+                    ->where('et.type', 1)
+                    ->orderBy('et.country', 'asc')
+                    ->first();
                 if (isset($emailSg1)) {
                     $emailg1 = explode(',', $emailSg1->email_gui);
                     if (count($emailg1) > 0) {
@@ -4526,11 +4526,11 @@ class FrontendController extends Controller
 
                 if ($subCategories && $subCategories->sub_pro_id) {
                     $emailSg3 = DB::table('email_notification as et')
-                    ->select('et.*')
-                    ->where('et.product_type', $subCategories->sub_pro_id)
-                    ->where('et.type', 3)
-                    ->orderBy('et.country', 'asc')
-                    ->first();
+                        ->select('et.*')
+                        ->where('et.product_type', $subCategories->sub_pro_id)
+                        ->where('et.type', 3)
+                        ->orderBy('et.country', 'asc')
+                        ->first();
                     if (isset($emailSg3)) {
                         $emailg3 = explode(',', $emailSg3->email);
                         if (count($emailg3) > 0) {
