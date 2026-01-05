8ad4747cb7a23aea0d832f1f0926179b09afca45
dan <dan@twjoin.com>
2026-01-05 23:21:02 +0800

    fix: downloadGui email failure

--- dependencies/app/Http/Controllers/FrontendController.php
+++ dependencies/app/Http/Controllers/FrontendController.php
@@ -4200,17 +4200,17 @@ class FrontendController extends Controller
 
                 DB::table('gui_downloads_email')->insert(
                     [
-                    'name' => $name,
-                    'tel' => $tel,
-                    'email' => $email,
-                    'company' => $company,
-                    'country' => $country,
-                    'filename' => $filename,
-                    'model' => $modelname,
-                    'type_name' => $typeName,
-                    'accept' => $acept,
-                    'created_at' => \Carbon\Carbon::now(),
-                ]
+                        'name' => $name,
+                        'tel' => $tel,
+                        'email' => $email,
+                        'company' => $company,
+                        'country' => $country,
+                        'filename' => $filename,
+                        'model' => $modelname,
+                        'type_name' => $typeName,
+                        'accept' => $acept,
+                        'created_at' => \Carbon\Carbon::now(),
+                    ]
                 );
 
                 if (1 == $acept) {
@@ -4253,8 +4253,9 @@ class FrontendController extends Controller
                     }
                 }
 
-                $email = Mail::to($emailsend)->send(new DowloadGui($request->except('_token')));
-                if (Mail::failures()) {
+                try {
+                    Mail::to($emailsend)->send(new DowloadGui($request->except('_token')));
+                } catch (\Exception $e) {
                     return \Redirect::back()->with('errorSendMail', 'ErorSendMail');
                 }
 
