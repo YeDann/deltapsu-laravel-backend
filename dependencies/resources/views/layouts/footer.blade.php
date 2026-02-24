42b6af6f9194c7602fa53b3face25918c13fff9c
dan <dan@twjoin.com>
2026-02-24 18:40:37 +0800

    feat: change Copyright Company name

--- dependencies/resources/views/layouts/footer.blade.php
+++ dependencies/resources/views/layouts/footer.blade.php
@@ -353,7 +353,7 @@
     </div>
     <div class="footer-nav">
         <div class="container">
-            <ul>Copyright © {{ date('Y') }} DeltaPSU. All Rights Reserved.</ul>
+            <ul>Copyright © {{ date('Y') }} Delta. All Rights Reserved.</ul>
             {{-- <ul>Designed by Degito</ul> --}}
         </div>
     </div>
