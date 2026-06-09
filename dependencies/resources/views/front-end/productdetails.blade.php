d5fb4245aae81252b66757418d6006f150b3d732
Alan Tsai <alan.tsai@twjoin.com>
2026-06-09 18:04:37 +0800

    fix: 麵包屑箭頭改 deltaww > 樣式、手風琴箭頭改細版、產品詳情手機按鈕間距一致

--- dependencies/resources/views/front-end/productdetails.blade.php
+++ dependencies/resources/views/front-end/productdetails.blade.php
@@ -1092,7 +1092,7 @@
                 <button class="btn btn-addcompare w-100 my-2"
                     onclick="showNavCoparison({{$product[0]['pro_id']}} ,{{$product[0]['cate_id']}})">{{$staticContent['Add_to_Compare']}}</button>
                 <a href="{{route('downloadFIle')}}/Datasheet/{{setTextpro($product[0]['pro_code'])}}" target="_blank">
-                    <button class="btn btn-datasheet w-100 mr-2">{{$staticContent['data_sheet']}}</button>
+                    <button class="btn btn-datasheet w-100 my-2">{{$staticContent['data_sheet']}}</button>
                 </a>
 
                 @foreach ($ec_link as $item)
