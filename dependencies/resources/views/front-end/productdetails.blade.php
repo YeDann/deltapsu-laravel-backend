a0f0ca262a064bdb3cf31407b98134c319396d24
Alan Tsai <alan.tsai@twjoin.com>
2026-07-06 18:08:51 +0800

    feat: 庫存扭暫時隱藏

--- dependencies/resources/views/front-end/productdetails.blade.php
+++ dependencies/resources/views/front-end/productdetails.blade.php
@@ -666,9 +666,10 @@
                                     class="btn img-btn-icon-pro tooltip2"><span>{{$staticContent['data_sheet']}}</span><img
                                         src="{{asset('/frontend-asset/image/Datasheet.svg')}}"></button>
                             </a>
+                            {{-- Stock Checking 暫時隱藏：正式站 netCOMPONENTS 白名單尚未核准，核准後移除本註解恢復
                             <button onclick="checkStock('{{$product[0]['pro_code']}}')"
                                     class="btn img-btn-icon-pro tooltip2"><span>{{$staticContent['Stock'] ?? 'Stock'}}</span><img
-                                        src="{{asset('/frontend-asset/image/Stock.svg')}}"></button>
+                                        src="{{asset('/frontend-asset/image/Stock.svg')}}"></button> --}}
 
                             @foreach ($ec_link as $item)
                             <a href="{{$item->link}}" target="_blank"><button
@@ -751,7 +752,8 @@
                             target="_blank">
                             <button class="btn btn-datasheet">{{$staticContent['data_sheet']}}</button>
                         </a>
-                        <button class="btn btn-datasheet" onclick="checkStock('{{$product[0]['pro_code']}}')">{{ $staticContent['Stock'] ?? 'Stock' }}</button>
+                        {{-- Stock Checking 暫時隱藏：正式站 netCOMPONENTS 白名單尚未核准，核准後移除本註解恢復
+                        <button class="btn btn-datasheet" onclick="checkStock('{{$product[0]['pro_code']}}')">{{ $staticContent['Stock'] ?? 'Stock' }}</button> --}}
                         @foreach ($ec_link as $item)
                         <a href="{{$item->link}}" target="_blank">
                             <button class="btn btn-buynow mr-2">{{$item->name}}</button>
@@ -1022,9 +1024,10 @@
                             target="_blank"><button
                                 class="btn img-btn-icon-pro tooltip2"><span>{{$staticContent['data_sheet']}}</span><img
                                     src="{{asset('/frontend-asset/image/Datasheet.svg')}}"></button></a>
+                        {{-- Stock Checking 暫時隱藏：正式站 netCOMPONENTS 白名單尚未核准，核准後移除本註解恢復
                         <button onclick="checkStock('{{$product[0]['pro_code']}}')"
                                 class="btn img-btn-icon-pro tooltip2"><span>{{$staticContent['Stock'] ?? 'Stock'}}</span><img
-                                    src="{{asset('/frontend-asset/image/Stock.svg')}}"></button>
+                                    src="{{asset('/frontend-asset/image/Stock.svg')}}"></button> --}}
                         @foreach ($ec_link as $item)
                         <a href="{{ $item->link }}" target="_blank"><button
                                 class="btn img-btn-icon-pro tooltip2"><span>{{ $item->name }}</span><img
@@ -1101,7 +1104,8 @@
                 <a href="{{route('downloadFIle')}}/Datasheet/{{setTextpro($product[0]['pro_code'])}}" target="_blank">
                     <button class="btn btn-datasheet w-100 my-2">{{$staticContent['data_sheet']}}</button>
                 </a>
-                <button class="btn btn-datasheet w-100 my-2" onclick="checkStock('{{$product[0]['pro_code']}}')">{{ $staticContent['Stock'] ?? 'Stock' }}</button>
+                {{-- Stock Checking 暫時隱藏：正式站 netCOMPONENTS 白名單尚未核准，核准後移除本註解恢復
+                <button class="btn btn-datasheet w-100 my-2" onclick="checkStock('{{$product[0]['pro_code']}}')">{{ $staticContent['Stock'] ?? 'Stock' }}</button> --}}
 
                 @foreach ($ec_link as $item)
                 <a href="{{$item->link}}" target="_blank">
