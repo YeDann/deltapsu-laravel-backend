ab607d14d8b76c4f8318d8157c3d3337b8037336
Alan Tsai <alan.tsai@joinx.co>
2026-09-15 16:32:08 +0800

    fix: reCAPTCHA loader 與後台 head 內容補上 CSP nonce

--- dependencies/app/Providers/AppServiceProvider.php
+++ dependencies/app/Providers/AppServiceProvider.php
@@ -29,10 +29,22 @@ class AppServiceProvider extends ServiceProvider
 
         // 用法：<script @cspNonce> ... </script>，輸出 nonce="xxx"。
         // nonce 由 ContentSecurityPolicy middleware 逐 request 產生。
-        // 現階段 CSP header 還沒放 nonce-xxx，所以這個屬性只是先標記、不影響行為；
-        // 等全站 inline script 標記完，才會把 nonce 寫進 header 並移除 'unsafe-inline'。
+        // CSP header 已移除 'unsafe-inline'，因此站內 inline <script> 一律要帶上本 directive，
+        // 會注入 inline script 的第三方 loader（GTM、reCAPTCHA）也要帶，否則注入的內容會被擋。
         Blade::directive('cspNonce', function () {
             return "<?php echo 'nonce=\"'.e(request()->attributes->get('csp_nonce')).'\"'; ?>";
+        });
+
+        // 用法：@cspHead($head)，輸出後台自填的 <head> 內容並補上 nonce。
+        // 這些內容（GSC 驗證碼、追蹤碼）存在 DB，不在程式碼內、無法逐一標記，
+        // 故於輸出時為沒有 src 也沒有 nonce 的 <script 補上本次請求的 nonce。
+        // 信任邊界不變：此欄位本就設計給管理員填任意 head 內容。
+        Blade::directive('cspHead', function ($expression) {
+            return "<?php echo preg_replace("
+                . "'/<script(?![^>]*\\\\bsrc=)(?![^>]*\\\\bnonce=)/i', "
+                . "'<script nonce=\"'.e(request()->attributes->get('csp_nonce')).'\"', "
+                . "{$expression} ?? ''"
+                . "); ?>";
         });
     }
 }
--- dependencies/resources/views/layouts/front-end404.blade.php
+++ dependencies/resources/views/layouts/front-end404.blade.php
@@ -332,7 +332,8 @@ $shareData->handle($request, function ($req) { return $req; });
     <script type="text/javascript" src="{{asset('/frontend-asset/js/slick.min.js')}}"></script>
     <script type="text/javascript" src="{{asset('/frontend-asset/js/zabuto_calendar.min.js')}}"></script>
     <script type="text/javascript" src="{{asset('/frontend-asset/js/mb5.js')}}"></script>
-    <script src="https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit" async defer>
+    {{-- nonce 不是為了放行 api.js 本身（網域已在白名單），而是讓 reCAPTCHA 傳遞給它執行期注入的 inline script；缺了會導致 onloadCallback 不執行 --}}
+    <script @cspNonce src="https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit" async defer>
     </script>
     <script @cspNonce>
         if ('loading' in HTMLImageElement.prototype) {
